ad95ab10e84183d0ec17d1d0648269f088b4dc7a
afesta12 <andrewfesta11@yahoo.com>
2023-10-23 23:03:37 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>City Parking</title>
+
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+</body>
+</html>
