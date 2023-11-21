731aced4e62ac3111af249a4e22ee219011c6d80
Maddi Lewis <93829615+4RCADIA@users.noreply.github.com>
2023-11-20 23:07:59 -0500

    improved reservation results page and made it easier to cancel a reservation

--- reservationResults.php
+++ reservationResults.php
@@ -18,9 +18,35 @@
     
 <main class="flex flex-col items-center justify-center h-screen w-screen bg-green-200">
 
-<div class="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="flex justify-center">
+            <img class="h-32 w-32" src="/images/wondervilleLogo.png" alt="">
+        </div>
+        <form action="" method="post" class="flex flex-col items-center">
+            <div class="relative overflow-x-auto">
+                <div class="flex items-center justify-between mb-4">
+                    <label for="confNum">Select a reservation to cancel (enter the associated confirmation number):</label>
+                    <select name="confNum" id="confNum" class="w-24 bg-white border border-gray-400 hover:border-gray-500 px-2 py-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                        <?php
+                            $cellphone = $_SESSION["cellphone"];
+                            $confirmation = $_SESSION["confirmation"];
+                            $sql = "SELECT ConformationNum
+                                FROM Reservation, User WHERE user.UNumber = Reservation.UNumber and (ConformationNum = '$confirmation' or  user.Phone = '$cellphone')";
+                            $result = $connection->query($sql);
 
-<div class="relative overflow-x-auto">
+                            if ($result) {
+                                while ($row = $result->fetch_assoc()) {
+                                    $confNum = $row["ConformationNum"];
+                                    echo "<option value='$confNum'>$confNum</option>";
+                                }
+                            }
+                        ?>
+                    </select>
+                    <button name="cancel" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
+                        Cancel Reservation
+                    </button>
+                </div>
+                </form>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 bg-green-200">
             <tr>
@@ -83,17 +109,48 @@
     }
 ?>
     </table>
+    <div class="mt-4 flex justify-center">
+                <a href="/index.php" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
+                    Return Home
+                </a>
     </div>
-</div>
-<div class="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <!-- Cancel reservation -->
-    <a href="/cancelReservation.php" class="text-center text-white bg-blue-500 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">
-                    Cancel A Reservation
-    </a>
 </div>
 </main>
 
 </body>
 </html>
 
-<
+<?php 
+    if(isset($_POST["cancel"])){
+        $confNum = $_POST["confNum"];
+        $cellphone = $_SESSION["cellphone"];
+        $confirmation = $_SESSION["confirmation"];
+
+        $sql1 = "SELECT Date FROM Reservation WHERE ConformationNum = $confNum";
+        $result1 = $connection->query($sql1);
+        $dateRow = $result1->fetch_assoc();
+        $date = $dateRow['Date'];
+
+        if ($result1) {
+            $resDate = new DateTime($date);
+            $currDate = new DateTime();
+
+            $currDate->sub(new DateInterval('P3D'));
+
+            // If at least 3 days before current date...
+            if ($currDate <= $resDate) {
+                // ... Get UNumber
+                $sql2 = "SELECT UNumber FROM Reservation WHERE Date = '$date'";
+                $result2 = $connection->query($sql2);
+                $row = $result2->fetch_assoc();
+                $userNum = $row['UNumber'];
+                // Update status to 'Cancelled'
+                $sql3 = "UPDATE Reservation SET Status = 'Cancelled' WHERE Date = '$date' and UNumber = $userNum";
+                $result3 = $connection->query($sql3);
+                echo '<script>alert("Reservation cancelled.")</script>';
+            } else { // ... else notify user
+                echo '<script>alert("Cancellations must be made at least 3 days in advance.")</script>';
+            }
+        }        
+    }
+?>
